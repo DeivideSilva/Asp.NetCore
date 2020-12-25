d397c5ddcf63a743ab70dbecb1bef0b51b1fb58a
Deivide Silva <37459501+DeivideSilva@users.noreply.github.com>
2020-12-25 18:05:33 -0300

    Exemplo de APi em C#

---
+++ ApiBiblioteca/BibliotecaAPI/Data/BibliotecaContexto.cs
@@ -0,0 +1,25 @@
+﻿using BibliotecaAPI.Data.Mapeamentos;
+using BibliotecaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotecaAPI.Data
+{
+    public class BibliotecaContexto : DbContext
+    {
+        public BibliotecaContexto(DbContextOptions<BibliotecaContexto> options) : base(options)
+        {
+
+        }
+
+       public DbSet<Editora> Editoras { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EditoraMap());
+        }
+    }
+}
---
+++ ApiBiblioteca/BibliotecaAPI/Data/Mapeamentos/EditoraMap.cs
@@ -0,0 +1,26 @@
+﻿using BibliotecaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace BibliotecaAPI.Data.Mapeamentos
+{
+    public class EditoraMap : IEntityTypeConfiguration<Editora>
+    {
+        public void Configure(EntityTypeBuilder<Editora> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Nome)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(t => t.Apresentacao)
+               .HasMaxLength(150)
+               .IsRequired(false);
+
+            builder.ToTable(nameof(Editora));
+
+        }
+    }
+}
