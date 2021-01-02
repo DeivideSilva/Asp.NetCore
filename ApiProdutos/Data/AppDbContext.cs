8157173c80a842052e68d599dffe714de0fd8e10
Deivide Silva <37459501+DeivideSilva@users.noreply.github.com>
2021-01-02 17:45:28 -0300

    Api usando EF Core5

---
+++ ApiProdutos/Data/AppDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiProdutos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base() 
+        { 
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Se não estiver configurado no projeto IU pega deginição de chame do json configurado
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(GetStringConectionConfig());
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        private string GetStringConectionConfig()
+        {
+            string strCon = "Data Source=DESKTOP-FA2LSQI\\SQLEXPRESS;Initial Catalog=Papelaria;Integrated Security=true";
+            return strCon;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>()
+             .Property(p => p.Nome)
+             .HasMaxLength(80);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Preco)
+                .HasPrecision(10,2);
+
+
+            modelBuilder.Entity<Produto>()
+                   .HasData(
+                    new Produto { Id = 1, Nome = "Caderno", Preco = 12.50M, Estoque = 100 },
+                    new Produto { Id = 2, Nome = "Caneta", Preco = 2.50M, Estoque = 50 },
+                    new Produto { Id = 3, Nome = "Apontador", Preco = 1.80M, Estoque = 80 }
+                );
+        }
+
+    }
+}
---
+++ ApiProdutos/Migrations/20210102191503_Inicial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiProdutos.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true),
+                    Preco = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    Estoque = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Estoque", "Nome", "Preco" },
+                values: new object[] { 1, 100, "Caderno", 12.50m });
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Estoque", "Nome", "Preco" },
+                values: new object[] { 2, 50, "Caneta", 2.50m });
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Estoque", "Nome", "Preco" },
+                values: new object[] { 3, 80, "Apontador", 1.80m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
