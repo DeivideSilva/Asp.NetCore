069f657179c8cb78c8d2c77b5399d0e6f98e4aa4
Deivide Silva <37459501+DeivideSilva@users.noreply.github.com>
2021-01-07 21:10:37 -0300

    Adiciona Repository na camada Infrastructure

---
+++ SGClientes/src/SGCliente.Infrastructure/Repository/EFRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGCliente.ApplicationCore.Interfaces.Repository;
+using SGCliente.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SGCliente.Infrastructure.Repository
+{
+    public class EFRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly ClienteContext _dbcontext;
+
+        public EFRepository(ClienteContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public TEntity Adicionar(TEntity entity)
+        {
+            _dbcontext.Set<TEntity>().Add(entity);
+            _dbcontext.SaveChanges();
+            return entity;
+        }
+
+        public void Atualizar(TEntity entity)
+        {
+            _dbcontext.Entry(entity).State = EntityState.Modified;
+            _dbcontext.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicado)
+        {
+            return _dbcontext.Set<TEntity>().Where(predicado).AsEnumerable();
+        }
+
+        public TEntity ObterPorId(int id)
+        {
+            return _dbcontext.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> ObterTodos()
+        {
+            return _dbcontext.Set<TEntity>().AsEnumerable();
+        }
+
+        public void Remover(TEntity entity)
+        {
+            _dbcontext.Set<TEntity>().Remove(entity);
+            _dbcontext.SaveChanges();
+        }
+    }
+}
