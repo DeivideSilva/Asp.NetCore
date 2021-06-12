9596820c877e2a98cb54aeaed4bbd1c3e839d986
Deivide <deivide1984@hotmail.com>
2021-06-12 20:20:11 -0300

    Inicio do projeto

---
+++ DigitalInnovationOne/Series/Classes/SerieRepositorio.cs
@@ -0,0 +1,39 @@
+﻿using Series.Interfaces;
+using System.Collections.Generic;
+
+namespace Series.Classes
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualizar(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Excluir(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Inserir(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornoPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
