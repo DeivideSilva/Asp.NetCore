70cc76dbbfcfa882bbd9eeeb0ddced2c05dbba11
Deivide <deivide1984@hotmail.com>
2021-06-11 21:33:54 -0300

    Exemplo de cadastro usando o console

---
+++ DigitalInnovationOne/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pilha = new Pilha();
+            pilha.Empilhar(1);
+            pilha.Empilhar(10);
+            pilha.Empilhar(100);
+            Console.WriteLine(pilha.Desempilhar());
+            Console.WriteLine(pilha.Desempilhar());
+            Console.WriteLine(pilha.Desempilhar());
+
+
+            //int[] a = new int[10];
+
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    a[i] = i * i;
+            //    Console.WriteLine($"a[{i}] = [a{i}]");
+            //}
+        }
+    }
+}
